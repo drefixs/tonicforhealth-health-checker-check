89a9a5b5a5ff68169658d918a8bb60e5f16ee266
Dmitry Gopkalo <drefixs@gmail.com>
2016-06-22 11:35:44 +0300

    - add getNestedDebug

--- src/CheckException.php
+++ src/CheckException.php
@@ -2,6 +2,8 @@
 
 namespace TonicHealthCheck\Check;
 
+use Exception;
+
 /**
  * Class CheckException
  */
@@ -22,5 +24,41 @@ class CheckException extends \Exception
             $code = -1;
         }
         parent::__construct(static::EXCEPTION_NAME.': '.$message, $code, $previous);
+    }
+
+    /**
+     * @param int $depth
+     * @return string
+     */
+    public function getNestedDebug($depth = -1)
+    {
+        return static::getDebug($this, $depth);
+    }
+
+    /**
+     * @param Exception $exception
+     *
+     * @return string
+     */
+    protected static function getDebug(Exception $exception, $depth = -1)
+    {
+        $errorDebugMsg = sprintf(
+            "ERROR CODE:%d\nERROR FILE:%s\nERROR MESSAGE:%s\nERROR TRACE:\n%s",
+            $exception->getCode(),
+            $exception->getFile(),
+            $exception->getMessage(),
+            $exception->getTraceAsString()
+        );
+
+        if (($depth == -1 || $depth > 0) && $exception->getPrevious() instanceof Exception) {
+            $depth -= $depth > 0 ? 1 : 0;
+            $errorDebugMsg = sprintf(
+                "%s\n\nPRIVIOUS EXCEPTION:\n%s",
+                $errorDebugMsg,
+                static::getDebug($exception->getPrevious(), $depth)
+            );
+        }
+
+        return $errorDebugMsg;
     }
 }
